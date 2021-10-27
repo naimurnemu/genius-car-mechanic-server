01482a2e523fd959ec8f924d97c2f48bfd9145ed
naimurnemu <naimurrahaman82@gmail.com>
2021-10-27 17:00:35 +0600

    done

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// connect to database
+// connect on database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.amqnd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
